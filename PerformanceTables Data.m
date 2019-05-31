0e05dc6c61356b97783185620d5b80c43647d0c2
Sam Jukes <44602949+SamJukesData@users.noreply.github.com>
2019-05-31 14:29:14 +0100

    Update PerformanceTables Data.m
    Add Keyword variable to make changing dataset easier to manage

--- PerformanceTables Data.m
+++ PerformanceTables Data.m
@@ -2,13 +2,16 @@ let
     // Get the data from Performce Tables
     YearRef = if Date.Month(DateTime.LocalNow()) >= 9 then Date.Year(DateTime.LocalNow()) - 1 else Date.Year(DateTime.LocalNow()) - 2,
     
+    // Change this to get different datasets
+    Keyword = "CENSUS"
+
     // Create URL for previous academic year
     // Edit uppercase keyword after &filters= to get different types of data
-    MainURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=CENSUS&fileformat=csv&year=" & Text.From(YearRef)&"-"&Text.From(YearRef + 1) & "&meta=false",
+    MainURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=csv&year=" & Text.From(YearRef)&"-"&Text.From(YearRef + 1) & "&meta=false",
     
     // Create URL for academic year two years ago
     // Edit uppercase keyword after &filters= to get different types of data
-    BackupURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=CENSUS&fileformat=csv&year=" & Text.From(YearRef - 1)&"-"&Text.From(YearRef) & "&meta=false",
+    BackupURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=csv&year=" & Text.From(YearRef - 1)&"-"&Text.From(YearRef) & "&meta=false",
     
     // Attempt to get data from MainURL
     ErrorValue = Value.Metadata(Web.Contents(MainURL, [ManualStatusHandling={404}]))[Response.Status],
