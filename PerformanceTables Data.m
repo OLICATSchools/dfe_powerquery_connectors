c3aeaaae6e46a483f37e26e0db769c2fb46d6aa7
S Jukes <SJukes@st-thomasmore.beds.sch.uk>
2019-06-12 15:58:08 +0100

    PerformanceTables Data - Replace collection method
    To enable support for all the data sources available via the compare school performance service, the target urls and data load methods needed to be changed to work with Excel files instead of csv

--- PerformanceTables Data.m
+++ PerformanceTables Data.m
@@ -1,5 +1,5 @@
 let
-    // Get the data from Performce Tables
+    // Get the data from Performce Tables in Excel file format
     YearRef = if Date.Month(DateTime.LocalNow()) >= 9 then Date.Year(DateTime.LocalNow()) - 1 else Date.Year(DateTime.LocalNow()) - 2,
     
     // Change this to get different datasets
@@ -7,16 +7,16 @@ let
 
     // Create URL for previous academic year
     // Edit uppercase keyword after &filters= to get different types of data
-    MainURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=csv&year=" & Text.From(YearRef)&"-"&Text.From(YearRef + 1) & "&meta=false",
+    MainURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=xls&year=" & Text.From(YearRef)&"-"&Text.From(YearRef + 1) & "&meta=false",
     
     // Create URL for academic year two years ago
     // Edit uppercase keyword after &filters= to get different types of data
-    BackupURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=csv&year=" & Text.From(YearRef - 1)&"-"&Text.From(YearRef) & "&meta=false",
+    BackupURL = "https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=" & Keyword & "&fileformat=xls&year=" & Text.From(YearRef - 1)&"-"&Text.From(YearRef) & "&meta=false",
     
     // Attempt to get data from MainURL
     ErrorValue = Value.Metadata(Web.Contents(MainURL, [ManualStatusHandling={404}]))[Response.Status],
     
     // If no data found at MainURL get data from BackupURL
-    Source = if ErrorValue = 404 then Csv.Document(Web.Contents(BackupURL),[Delimiter=",", Encoding=1252, QuoteStyle=QuoteStyle.None]) else Csv.Document(Web.Contents(MainURL),[Delimiter=",", Encoding=1252, QuoteStyle=QuoteStyle.None])
+    Source = if ErrorValue = 404 then Excel.Workbook(Web.Contents(BackupURL)) else Excel.Workbook(Web.Contents(MainURL))
 in
     Source
